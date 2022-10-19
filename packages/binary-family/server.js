5bd9102d8fee5b36ff82c132ba88eceb1a735425
xinghunm <xinghunm@keyayun.com>
2022-10-19 19:11:36 +0800

    add examples

--- packages/binary-family/server.js
+++ packages/binary-family/server.js
@@ -5,15 +5,24 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '')));
-app.use(bodyParser.text());
-
-app.post('/upload', function (req, res) {
-  console.log('body', req.body);
-});
+// app.use(bodyParser.text());
 
 app.get('/ajax', function (req, res) {
   console.log('/ajax');
   res.send('hello world');
+});
+
+app.post('/upload', function (req, res) {
+  const chunks = [];
+
+  req.on('data', (buf) => {
+    chunks.push(buf);
+  });
+  req.on('end', () => {
+    let buffer = Buffer.concat(chunks);
+    console.log(buffer.toString());
+    res.sendStatus(200);
+  });
 });
 
 app.listen(8081, function () {
